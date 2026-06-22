16baa7d9cc9f39a4c0accf9ea6317f0e864afb2a
Nemo <itsnemogames@gmail.com>
2026-06-22 18:38:05 -0230

    Fix: generate multi-size ICO for NSIS compatibility

--- scripts/build-icons.js
+++ scripts/build-icons.js
@@ -2,18 +2,29 @@ const sharp = require('sharp')
 const { default: pngToIco } = require('png-to-ico')
 const path = require('path')
 const fs = require('fs')
+const os = require('os')
 
 async function main() {
-  const svgPath  = path.join(__dirname, '../build/icon.svg')
-  const pngPath  = path.join(__dirname, '../build/icon.png')
-  const icoPath  = path.join(__dirname, '../build/icon.ico')
+  const svgPath = path.join(__dirname, '../build/icon.svg')
+  const buildDir = path.join(__dirname, '../build')
 
-  await sharp(svgPath).resize(512, 512).png().toFile(pngPath)
+  // 512x512 PNG for electron-builder and display
+  await sharp(svgPath).resize(512, 512).png().toFile(path.join(buildDir, 'icon.png'))
   console.log('  icon.png generated')
 
-  const icoBuffer = await pngToIco([pngPath])
-  fs.writeFileSync(icoPath, icoBuffer)
-  console.log('  icon.ico generated')
+  // Multi-size ICO required by NSIS (16, 32, 48, 256)
+  const tmpDir = os.tmpdir()
+  const sizes = [16, 32, 48, 256]
+  const pngPaths = []
+  for (const size of sizes) {
+    const p = path.join(tmpDir, `icon-${size}.png`)
+    await sharp(svgPath).resize(size, size).png().toFile(p)
+    pngPaths.push(p)
+  }
+
+  const icoBuffer = await pngToIco(pngPaths)
+  fs.writeFileSync(path.join(buildDir, 'icon.ico'), icoBuffer)
+  console.log('  icon.ico generated (16, 32, 48, 256px)')
 }
 
 main().catch(err => {
